281968f829d932734c05ffe671dfb35e22643036
Andreas Nikolaou <andnik1998@gmail.com>
2020-06-22 14:13:51 +0100

    added event on the clikc of the button

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,9 +8,14 @@ export class ServerComponent {
     serverId = 116;
     serverStatus = 'online';
     allowNewServer = false;
+    serverCreationStatus = "No server was created!";
 
     constructor() {
         setTimeout(() => {this.allowNewServer=true}, 2000);
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = "Server was created!";
     }
 
     getServerStatus(){
