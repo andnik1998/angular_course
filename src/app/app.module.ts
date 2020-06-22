6ca2a7f1aff9d05979b61f7bbf643ab946ea7569
Andreas Nikolaou <andnik1998@gmail.com>
2020-06-22 12:16:42 +0100

    added logic in TS and displayed it in HTML of server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
     SuccessAlertComponent
   ],
   imports: [
